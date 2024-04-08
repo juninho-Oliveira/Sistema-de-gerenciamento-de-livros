62db5eacfccbe89af1cd167584631ad307416fbb
impressao recordalbum <junior.oliveira05@icloud.com>
2024-04-08 16:37:23 -0300

    add nomes

--- scripts/update.js
+++ scripts/update.js
@@ -1,11 +1,10 @@
-/*function EditarSeries(id, series) {
+function EditarSeries(id, livros) {
 
   //alert(`ola estou aqui ${}` );
   const ids = id;
-  const nomeSerie = series;
+  const nomeUser = livros;
   localStorage.setItem('meu id', ids)
-  localStorage.setItem('meu nome', nomeSerie.nomeSeries)
+  localStorage.setItem('meu nome', nomeUser.NomeUsuario)
 
-  window.location.href = "../Editar/editar.html"
+  window.location.href = "../usuario/usuario.html"
 }
-*/
